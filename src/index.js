8e3eaa1af76089cfdcc65c6a8de2c018edb55ada
kir-22 <kirill89kireew@gmail.com>
2018-03-01 17:56:44 +0300

    Made

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  // MY implementation
+    var count=0;
+    for(var i=5;number/i>=1;i*=5){
+        count+=Math.floor(number/i)
+    }
+    return count;
+
 }
